6fe9006d94a0b54dd232bbf90220e515cea8cbba
GZTime <Time.GZ@outlook.com>
2022-11-29 05:03:59 +0800

    dev: set up dev container

--- bridge/build.rs
+++ bridge/build.rs
@@ -2,6 +2,8 @@ fn main() {
     cxx_build::bridge("src/bridge.rs")
         .file("src/handler/ha_wapper.cc")
         .flag_if_supported("-std=c++2a")
+        .include("include/mysql")
+        .include("include/mysql/include")
         .compile("ha_wapper");
 
     println!("cargo:rerun-if-changed=src/main.rs");
--- bridge/include/ha_wapper.hpp
+++ bridge/include/ha_wapper.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
 #include <cstdio>
-#include "handler.hpp"
+// #include "mysql/sql/handler.h"
 
 void hello_world();
