f8c41015ec1752702ca4b624ded537ac10c619a1
GZTimeWalker <gztimewalker@163.com>
2022-12-12 11:08:42 +0000

    wip: update test script

--- scripts/sql.sh
+++ scripts/sql.sh
@@ -18,15 +18,17 @@ mysql -uroot -e "CREATE TABLE IF NOT EXISTS testdb.test (
     id INT NOT NULL,
     num DOUBLE NOT NULL DEFAULT 2.3,
     date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    name VARCHAR(10) NOT NULL
+    name VARCHAR(10) NOT NULL,
+    other TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
 ) ENGINE=yydb"
 
-mysql -uroot -e "INSERT INTO testdb.test (id, name) VALUES
-    (1, 'test_1_1'),
-    (4, 'test_4')"
+mysql -uroot -e "INSERT INTO testdb.test (id, name, num) VALUES
+    (1, 'test_1_1', 23232.2), (4, 'test_4', 13.32)"
 
-mysql -uroot -e "INSERT INTO testdb.test (id, name) VALUES (5, 'test__8')"
+mysql -uroot -e "INSERT INTO testdb.test (id, name, other) VALUES
+    (5, 'test__8', 'okk! 你好啊！🙈okk! 你好啊！🙈okk! 你好啊！🙈okk! 你好啊！🙈okk! 你好啊！🙈'),
+    (7, 'test?', 'no-okk 我不好👎 no-okk 我不好👎 no-okk 我不好👎 no-okk 我不好👎 no-okk 我不好👎')"
 
-mysql -uroot -e "UPDATE testdb.test SET name = 'test_??_5'"
+mysql -uroot -e "UPDATE testdb.test SET name = 'test__8'"
 
 mysql -uroot -e "SELECT * FROM testdb.test"
