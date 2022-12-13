f58407695e07c7da9c09ccea7232c14bd8f9a566
sumuzhe317 <1051655293@qq.com>
2022-12-13 13:34:21 +0000

    wip:update

--- scripts/initdb.sh
+++ scripts/initdb.sh
@@ -7,7 +7,7 @@ export MYSQL_BASE_DIR=/usr/local/mysql
 $MYSQL_BASE_DIR/bin/mysqld --initialize --user=mysql
 $MYSQL_BASE_DIR/bin/mysql_ssl_rsa_setup
 
-sudo chown mysql:mysql -R /usr/local/mysql/data
+sudo chown dev:dev -R /usr/local/mysql/data
 
 echo "Use mysql -uroot -p to login to the mysql"
 echo "Use set password for 'root'@'localhost' = ''; to remove password."
